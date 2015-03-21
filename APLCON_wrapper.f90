d094d8b5e1facc6ea091ffeb4fa49329dc2efecc
Andreas Neiser <andreas.neiser@gmail.com>
2015-03-22 00:49:30 +0100

    All routines have interface now, but most are untested

--- APLCON_wrapper.f90
+++ APLCON_wrapper.f90
@@ -3,6 +3,146 @@ module APLCON_wrapper
   implicit none
 contains
 
+  ! main routines
+  subroutine C_APLCON_APLCON(NVAR,MCST) bind(c)
+    integer(c_int), value, intent(in) :: NVAR, MCST
+    CALL APLCON(NVAR,MCST)
+  end subroutine C_APLCON_APLCON
+
+  subroutine C_APLCON_APLOOP(X,VX,F,IRET) bind(c)
+    real(c_double), dimension(*), intent(inout) :: X,VX,F
+    integer(c_int), intent(out) :: IRET
+    CALL APLOOP(X,VX,F,IRET)
+  end subroutine C_APLCON_APLOOP
+
+  subroutine C_APLCON_SIMSEL(X,VX,NY,LIST,Y,VY) bind(c)
+    real(c_double), dimension(*), intent(in) :: X,VX,LIST
+    integer(c_int), value, intent(in) :: NY
+    real(c_double), dimension(*), intent(out) :: Y,VY
+    CALL SIMSEL(X,VX,NY,LIST,Y,VY)
+  end subroutine C_APLCON_SIMSEL
+
+  subroutine C_APLCON_SIMTRN(X,VX,NX) bind(c)
+    real(c_double), dimension(*), intent(inout) :: X,VX
+    integer(c_int), value, intent(in) :: NX
+    CALL SIMTRN(X,VX,NX)
+  end subroutine C_APLCON_SIMTRN
+
+  ! printout related routines (useful for debugging)
+  subroutine C_APLCON_APNAME(I,NAME) bind(c)
+    integer(c_int), value, intent(in) :: I
+    character(kind=c_char,len=1), intent(in) :: NAME(*)
+    CALL APNAME(I,c_to_f_string(NAME))
+  end subroutine C_APLCON_APNAME
+
+  subroutine C_APLCON_CIPRV(LUP,X,VX,N) bind(c)
+    integer(c_int), value, intent(in) :: LUP, N
+    real(c_double), dimension(*), intent(in) :: X, VX
+    CALL CIPRV(LUP,X,VX,N)
+  end subroutine C_APLCON_CIPRV
+
+  subroutine C_APLCON_CFPRV(LUP,X,VX,N) bind(c)
+    integer(c_int), value, intent(in) :: LUP, N
+    real(c_double), dimension(*), intent(in) :: X, VX
+    CALL CFPRV(LUP,X,VX,N)
+  end subroutine C_APLCON_CFPRV
+
+  subroutine C_APLCON_CFCORR(LUP,V,N) bind(c)
+    integer(c_int), value, intent(in) :: LUP, N
+    real(c_double), dimension(*), intent(in) :: V
+    CALL CFCORR(LUP,V,N)
+  end subroutine C_APLCON_CFCORR
+
+  ! ENTRY points in aplusopt.F
+  ! mainly setup/config routines
+  ! not all are mentioned in the README
+  subroutine C_APLCON_APRINT(LUNP,IPR) bind(c)
+    integer(c_int), value, intent(in) :: LUNP, IPR
+    CALL APRINT(LUNP,IPR)
+  end subroutine C_APLCON_APRINT
+
+  subroutine C_APLCON_APDEPS(EPSF) bind(c)
+    real(c_double), value, intent(in) :: EPSF
+    CALL APDEPS(EPSF)
+  end subroutine C_APLCON_APDEPS
+
+  subroutine C_APLCON_APDERF(DERFAC) bind(c)
+    real(c_double), value, intent(in) :: DERFAC
+    CALL APDERF(DERFAC)
+  end subroutine C_APLCON_APDERF
+
+  subroutine C_APLCON_APDERU(DERUFC) bind(c)
+    real(c_double), value, intent(in) :: DERUFC
+    CALL APDERU(DERUFC)
+  end subroutine C_APLCON_APDERU
+
+  subroutine C_APLCON_APDLOW(DERLOW) bind(c)
+    real(c_double), value, intent(in) :: DERLOW
+    CALL APDLOW(DERLOW)
+  end subroutine C_APLCON_APDLOW
+
+  subroutine C_APLCON_APITER(ITERMX) bind(c)
+    integer(c_int), value, intent(in) :: ITERMX
+    CALL APITER(ITERMX)
+  end subroutine C_APLCON_APITER
+
+  subroutine C_APLCON_APROFL(I1,I2) bind(c)
+    integer(c_int), value, intent(in) :: I1, I2
+    CALL APROFL(I1,I2)
+  end subroutine C_APLCON_APROFL
+
+  subroutine C_APLCON_APSTEP(I,STEP) bind(c)
+    integer(c_int), value, intent(in) :: I
+    real(c_double), value, intent(in) :: STEP
+    CALL APSTEP(I,STEP)
+  end subroutine C_APLCON_APSTEP
+
+  subroutine C_APLCON_APFIX(I) bind(c)
+    integer(c_int), value, intent(in) :: I
+    CALL APFIX(I)
+  end subroutine C_APLCON_APFIX
+
+  subroutine C_APLCON_APLIMT(I,XLOW,XHIG) bind(c)
+    integer(c_int), value, intent(in) :: I
+    real(c_double), value, intent(in) :: XLOW, XHIG
+    CALL APLIMT(I,XLOW,XHIG)
+  end subroutine C_APLCON_APLIMT
+
+  subroutine C_APLCON_APTRIN(I) bind(c)
+    integer(c_int), value, intent(in) :: I
+    CALL APTRIN(I)
+  end subroutine C_APLCON_APTRIN
+
+  subroutine C_APLCON_APOISS(I) bind(c)
+    integer(c_int), value, intent(in) :: I
+    CALL APOISS(I)
+  end subroutine C_APLCON_APOISS
+
+  subroutine C_APLCON_ABINOM(I) bind(c)
+    integer(c_int), value, intent(in) :: I
+    CALL ABINOM(I)
+  end subroutine C_APLCON_ABINOM
+
+  subroutine C_APLCON_APLOGN(I) bind(c)
+    integer(c_int), value, intent(in) :: I
+    CALL APLOGN(I)
+  end subroutine C_APLCON_APLOGN
+
+  subroutine C_APLCON_APSQRT(I) bind(c)
+    integer(c_int), value, intent(in) :: I
+    CALL APSQRT(I)
+  end subroutine C_APLCON_APSQRT
+
+  subroutine C_APLCON_APOWER(I) bind(c)
+    integer(c_int), value, intent(in) :: I
+    CALL APOWER(I)
+  end subroutine C_APLCON_APOWER
+
+  subroutine C_APLCON_APOSIT(I) bind(c)
+    integer(c_int), value, intent(in) :: I
+    CALL APOSIT(I)
+  end subroutine C_APLCON_APOSIT
+
   ! helper function to convert c string to fortran string
   function c_to_f_string(s) result(str)
     character(kind=c_char,len=1), intent(in) :: s(*)
@@ -18,44 +158,4 @@ contains
     str = transfer(s(1:nchars), str)
   end function c_to_f_string
 
-  subroutine C_APLCON_APLCON(NVAR,MCST) bind(c)
-    integer(c_int), value, intent(in) :: NVAR, MCST
-    CALL APLCON(NVAR,MCST)
-  end subroutine C_APLCON_APLCON
-
-  subroutine C_APLCON_APRINT(LUNP,IPR) bind(c)
-    integer(c_int), value, intent(in) :: LUNP, IPR
-    CALL APRINT(LUNP,IPR)
-  end subroutine C_APLCON_APRINT
-  
-  subroutine C_APLCON_APNAME(I,NAME) bind(c)
-    integer(c_int), value, intent(in) :: I
-    character(kind=c_char,len=1), intent(in) :: NAME(*)
-    CALL APNAME(I,c_to_f_string(NAME))
-  end subroutine C_APLCON_APNAME
-
-  subroutine C_APLCON_CIPRV(LUP,X,VX,N) bind(c)
-    integer(c_int), value, intent(in) :: LUP, N
-    real(c_double), dimension(*), intent(in) :: X, VX    
-    CALL CIPRV(LUP,X,VX,N)
-  end subroutine C_APLCON_CIPRV
-
-  subroutine C_APLCON_CFCORR(LUP,V,N) bind(c)
-    integer(c_int), value, intent(in) :: LUP, N
-    real(c_double), dimension(*), intent(in) :: V
-    CALL CFCORR(LUP,V,N)
-  end subroutine C_APLCON_CFCORR
-
-  subroutine C_APLCON_APROFL(I1,I2) bind(c)
-    integer(c_int), value, intent(in) :: I1, I2
-    CALL APROFL(I1,I2)
-  end subroutine C_APLCON_APROFL
-
-  subroutine C_APLCON_APLOOP(X,VX,F,IRET) bind(c)
-    real(c_double), dimension(*), intent(inout) :: X,VX,F
-    integer(c_int), intent(out) :: IRET
-    CALL APLOOP(X,VX,F,IRET)
-  end subroutine C_APLCON_APLOOP
-
-  
 end module APLCON_wrapper
--- APLCON_wrapper.h
+++ APLCON_wrapper.h
@@ -1,12 +1,35 @@
 #ifndef __APLCON_wrapper_h__
 #define __APLCON_wrapper_h__
 
-void c_aplcon_aplcon(int NVAR, int MCST);
-void c_aplcon_aprint(int LUNP, int IPR);
-void c_aplcon_apname(int I, const char* NAME);
-void c_aplcon_ciprv(int LUP, const double X[], const double VX[], int N);
-void c_aplcon_cfcorr(int LUP, const double V[], int N);
-void c_aplcon_aprofl(int I1, int I2);
+// main routines
+void c_aplcon_aplcon(const int NVAR, const int MCST);
 void c_aplcon_aploop(double X[], double VX[], double F[], int* IRET);
+void c_aplcon_simsel(const double X[], const double VX[], const int NY, const int LIST[], double Y[], double VY[]);
+void c_aplcon_simtrn(double X[], double VX[], const int NX);
+
+// printout related routines (useful for debugging)
+void c_aplcon_apname(const int I, const char* NAME);
+void c_aplcon_ciprv(const int LUP, const double X[], const double VX[], const int N);
+void c_aplcon_cfprv(const int LUP, const double X[], const double VX[], const int N);
+void c_aplcon_cfcorr(const int LUP, const double V[], const int N);
+
+// setup/config routines
+void c_aplcon_aprint(const int LUNP, const int IPR);
+void c_aplcon_apdeps(const double EPSF);
+void c_aplcon_apderf(const double DERFAC);
+void c_aplcon_apderu(const double DERUFC);
+void c_aplcon_apdlow(const double DERLOW);
+void c_aplcon_apiter(const int ITERMX);
+void c_aplcon_aprofl(const int I1, const int I2);
+void c_aplcon_apstep(const int I, const double STEP);
+void c_aplcon_apfix(const int I);
+void c_aplcon_aplimt(const int I, const double XLOW, const double XHIG);
+void c_aplcon_aptrin(const int I);
+void c_aplcon_apoiss(const int I);
+void c_aplcon_abinom(const int I);
+void c_aplcon_aplogn(const int I);
+void c_aplcon_apsqrt(const int I);
+void c_aplcon_apower(const int I);
+void c_aplcon_aposit(const int I);
 
 #endif
