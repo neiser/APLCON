878f9cdb38e54d69b9e3fe92c6bcb4f53f3841e6
Andreas Neiser <andreas.neiser@gmail.com>
2015-03-28 07:27:31 +0100

    Improved GNU CXX/Fortran compiler flags

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -1,12 +1,11 @@
 # really no optimization in debug mode
-if(CMAKE_COMPILER_IS_GNUCXX)
-  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -Wall -Wextra")
+if(CMAKE_CXX_COMPILER_ID STREQUAL GNU AND
+    CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -Wall -Wextra")
 else()
-  message(ERROR "So far only gcc supported")
+  message(FATAL_ERROR "Only GNU CXX/Fortran compiler supported")
 endif()
-
-#set(CMAKE_Fortran_FLAGS "-fPIC")
 
 # set default build type if unspecified so far
 if(NOT CMAKE_BUILD_TYPE)
@@ -16,4 +15,5 @@ endif()
 
 # figure out compile flags
 string(TOUPPER ${CMAKE_BUILD_TYPE} BUILD_TYPE)
-set(DEFAULT_COMPILE_FLAGS ${CMAKE_CXX_FLAGS_${BUILD_TYPE}})
+set(DEFAULT_CXX_COMPILE_FLAGS ${CMAKE_CXX_FLAGS_${BUILD_TYPE}})
+set(DEFAULT_Fortran_COMPILE_FLAGS ${CMAKE_Fortran_FLAGS_${BUILD_TYPE}})
