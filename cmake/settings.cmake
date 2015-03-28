9d286228819c3393bd34d3b13d13cfef8bb59dd7
Andreas Neiser <andreas.neiser@gmail.com>
2015-03-28 17:55:00 +0100

    Improve compiler flag handling in cmake

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -2,7 +2,11 @@
 if(CMAKE_CXX_COMPILER_ID STREQUAL GNU AND
     CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -Wall -Wextra")
+  set(DEBUG_FLAGS "-O0 -Wall -Wextra")
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${DEBUG_FLAGS}")
+  set(CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} ${DEBUG_FLAGS}")
+  # suppress warnings in release mode (just not to confuse people)
+  set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} -w")
 else()
   message(FATAL_ERROR "Only GNU CXX/Fortran compiler supported")
 endif()
