41decb1bdba9a0e7706f4b8c5aec3621a1347835
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-03-30 17:21:00 +0200

    Free functions in library headers are evil

--- src/detail/APLCON_hpp.hpp
+++ src/detail/APLCON_hpp.hpp
@@ -116,40 +116,5 @@ struct build_indices<0, Is...> : indices<Is...> {};
 
 } // end namespace APLCON_
 
-
-
-
-
-// helpful for debugging type matching
-#ifdef __GNUG__
-#include <cstdlib>
-#include <memory>
-#include <cxxabi.h>
-#include <string>
-#endif
-
-namespace APLCON_ {
-#ifdef __GNUG__
-std::string demangle(const char* name) {
-
-  int status = -4; // some arbitrary value to eliminate the compiler warning
-
-  // enable c++11 by passing the flag -std=c++11 to g++
-  std::unique_ptr<char, void(*)(void*)> res {
-    abi::__cxa_demangle(name, NULL, NULL, &status),
-        std::free
-  };
-
-  return (status==0) ? res.get() : name ;
-}
-#else
-// does nothing if not g++
-std::string demangle(const char* name) {
-  return name;
-}
-#endif
-
-}
-
-#endif
+#endif // _APLCON_APLCON_HPP_HPP
 
